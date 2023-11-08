feb83b3b9bd3a931da5679132f969d11861ae51e
Isaac Clinton <apidaisaac@gmail.com>
2023-11-08 18:42:03 +0300

    Added verification page UI

--- lib/verification_page/view/verification_page.dart
+++ lib/verification_page/view/verification_page.dart
@@ -1,4 +1,6 @@
+import 'package:check_in/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_verification_code_field/flutter_verification_code_field.dart';
 
 class VerificationPage extends StatelessWidget {
   const VerificationPage({super.key});
@@ -11,16 +13,42 @@ class VerificationPage extends StatelessWidget {
         child: SizedBox(
           height: size.height,
           width: size.width,
-          child: const Column(
-            children: [
-              SizedBox(height: 20),
-              Text(
-                "Verify email address",
-                style: TextStyle(
-                  fontSize: 20,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 20),
+                const Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Verify email address",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 40),
+                VerificationCodeField(length: 6),
+                const SizedBox(height: 40),
+                const Text("50s remaining"),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                  ),
+                  child: const Text("Resend Code"),
+                ),
+                const SizedBox(height: 40),
+                Align(
+                  alignment: Alignment.center,
+                  child: CustomButton(
+                    "VERIFY",
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
